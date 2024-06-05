a048d023c2a2064f97f59895db6b28e9916af636
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-05 09:02:02 -0500

    feat: :sparkles: POST Posts
    Validamos lo datos y realizamos le post de posts
    
    chore: :truck: Cambiamos los endpoints de Posts

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,12 @@
-import { addAlbum, getAllAlbums } from "./module/album.js";
+import { addAlbum } from "./module/album.js";
 import { addPost } from "./module/posts.js";
 
 let user = parseInt(prompt("Give me the user number"))
 let title = prompt("Give me the album title" )
+let body = prompt("Give me the short post description")
 
 
-console.table(await addAlbum ({
+console.table(await addPost ({
 
     "userId": user,
     "title": title,
---
+++ Js/module/posts.js
@@ -0,0 +1,40 @@
+import { getUser } from "./user.js" 
+
+// VALIDAR DATOS
+const validatePost = async({userId, title, body}) => {
+
+    if (typeof userId !== "number" || userId === undefined) {
+        return { status: 406, message: "Invalid data format: userId must be a number." };
+    }   
+    let user = await getUser({ userId });
+    if (user.status === 404) {  
+        return { status: 200, message: "The User doesn't exist" };
+    }
+    if (typeof title !== "string" || title === undefined) {
+        return { status: 406, message: "Invalid data format: title must be a string." };
+    }
+    if (typeof body !== "string" || body === undefined) {
+        return { status: 406, message: "Invalid data format: body must be a string." };
+    }
+
+} 
+
+
+// POST
+export const addPost = async(arg) => {
+
+    let val = await validatePost(arg);
+    if (val) {
+        return val;
+    }
+
+    const config = {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(arg)
+    }
+    let res = await fetch ("https://jsonplaceholder.typicode.com/posts", config);
+    let data = await res.json();
+    return data;
+
+}
