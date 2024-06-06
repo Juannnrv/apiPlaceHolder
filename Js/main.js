307ea965bbed38c51a4d98fef42d144331f71823
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-06 14:52:05 -0500

    feat: :sparkles: Menú y delete Posts

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,5 @@
 import { addAlbum, deleteAlbum, getAlbum } from "./module/album.js";
-import { addPost } from "./module/posts.js";
+import { addPost, deletePosts, getPost } from "./module/posts.js";
 import { addUser } from "./module/user.js";
 import { addComment } from "./module/comments.js"; 
 import { addPhoto } from "./module/photos.js";
@@ -11,23 +11,67 @@ let menuAlbums = async() => {
 
 -------- MODULES --------  
 
-  1 - Posts
-  2 - Comments
-  3 - Albums
-  4 - Photos
-  5 - Users
+  1 - Posts 📬
+  2 - Comments 📋
+  3 - Albums 📚
+  4 - Photos 🖼️
+  5 - Users 👪
 
   `))
+  if (menu === 1) {
 
-  if(menu === 3) {
+    let menuPosts = parseInt(prompt(`
+
+--------- POSTS ---------  
+
+  1 - Add Posts 🚀
+  2 - Delete Posts 🚮
+  3 - Search Posts 🔍
+  4 - Back to main menu
+
+  `))
+  if (menuPosts === 1) {
+
+    let user = parseInt(prompt("Give me the user number"))
+    let title = prompt("Give me the album title" )
+    let body = prompt("Give me the short post description")
+
+    console.table( await addPost({
+
+      "userId": user,
+      "title": title,
+      "body": body
+
+    }) )
+
+  }
+  else if (menuPosts === 2) {
+
+    let postId = prompt("Indicate the Post Id you want to delete");
+    console.table( await deletePosts(postId));
+
+  }
+  else if (menuPosts === 3) {
+
+    let postId = prompt("Give me the Album Id you want to search: ")
+    console.table(await getPost(postId));
+
+  }
+
+  } 
+
+
+
+  // ALBUMS
+  else if(menu === 3) {
 
     let menuAlbum = parseInt(prompt(`
 
 --------- ALBUMS ---------  
 
-  1 - Add Album
-  2 - Delete Album
-  3 - Search ALbum
+  1 - Add Album 🚀
+  2 - Delete Album 🚮
+  3 - Search ALbum 🔍
   4 - Back to main menu
 
   `))
@@ -52,8 +96,10 @@ let menuAlbums = async() => {
 
   }
   else if (menuAlbum === 3) {
+    
     let albumId = prompt("Give me the Album Id you want to search: ")
     console.table(await getAlbum(albumId));
+
   }
 
   }
--- Js/module/posts.js
+++ Js/module/posts.js
@@ -3,7 +3,7 @@ import { getUser } from "./user.js"
 // GET
 export const getPost = async({postId}) => {
 
-    let res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    let res = await fetch(`http://172.16.101.146:5800/posts/${postId}`)
 
     if (!res.ok) {
         return { status: 404, message: "Post doesn't exist" };
@@ -47,8 +47,41 @@ export const addPost = async(arg) => {
         headers: {"content-type": "application/json"},
         body: JSON.stringify(arg)
     }
-    let res = await fetch ("https://jsonplaceholder.typicode.com/posts", config);
+    let res = await fetch ("http://172.16.101.146:5800/posts", config);
     let data = await res.json();
+    return data;
+
+}
+
+// DELETE
+const validateDeletePosts = async({id}) => {
+
+    if (typeof id !== "string" || id  === undefined) { 
+        return { status: 406, message: "Invalid data format: postId must be a string" };
+    }
+
+}
+
+export const deletePost = async (arg) => {
+
+    let val = await validateDeletePosts(arg);
+    if (val) return val;
+
+    let config = {
+
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" }
+
+    }
+
+    let res = await fetch(`http://172.16.101.146:5800/posts/${arg.id}`, config);
+    if (res.status === 404) {
+        return { status: 404, message: "The Post you want to delete isn't registered in the data" };
+    }
+
+    let data = await res.json();
+    data.status = 202;
+    data.message = `The Post ${arg.id} was deleted successfully from the database`;
     return data;
 
 }
