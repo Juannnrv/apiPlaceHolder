4e21296f74bf3180e29a6d1f56220ccc47de2a42
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-07 10:17:20 -0500

    feat: :sparkles: PATCH Photos

--- Js/module/photos.js
+++ Js/module/photos.js
@@ -1,4 +1,8 @@
 import { getAlbum } from "./album.js";
+import { remoto, local } from "./endopoints.js"
+import { getUser } from "./user.js";
+
+const enlace = local();
 
 // Validaciones 
 const validatePhoto = async(photo, albumId) => {
@@ -34,8 +38,9 @@ export const addPhoto = async(arg) => {
         body: JSON.stringify(arg)
     }
     
-    let res = await fetch ("http://172.16.101.146:5803/photos", config);
+    let res = await fetch (`${enlace.photo}`, config);
     let data = await res.json();
+    alert("Photo added succesfully");
     return data;
 
 }
@@ -43,7 +48,7 @@ export const addPhoto = async(arg) => {
 // GET
 export const getPhoto = async(photoId) => {
 
-    let res = await fetch(`http://172.16.101.146:5803/photos/${photoId}`);
+    let res = await fetch(`${enlace.photo}/${photoId}`);
 
     if (res.status === 404) {
         return { status: 404, message: "Photo doesn't exist" };
@@ -75,45 +80,59 @@ export const deletePhoto = async(arg) => {
 
     }
 
-    let res = await fetch(`http://172.16.101.146:5803/photos/${arg.id}`, config);
+    let res = await fetch(`${enlace.photo}/${arg.id}`, config);
     if (res.status === 404) {
         return { status: 404, message: "The photo you want to delete doesn't exist in the data" };
     }
 
     let data = await res.json();
-    data.status = 202
-    data.message = `The photo ${arg.id} was deleted successfully`
+    alert("Photo deleted succesfully");
     return data;
 
 }
 
-// PUT 
+// PATCH
 export const updatePhoto = async(id) => {
 
     let photo = await getPhoto(id);
-    if (photo.status == 204) return "Photo not found";
+    if (photo.status === 204) return "Photo not found";
 
     const key = Object.keys(photo).filter(key => key !== "id");
     let opciones = key.map((key, value) => `${value + 1}. ${key}`).join("\n");
 
-    let opc = prompt(`Available options: \n${opciones}\n  Give me the option:`);
+    let opc = prompt(`Available options: \n\n${opciones}\n\n  Give me the option:`);
     
     let newKey = key[opc - 1];
     if (!newKey) return "Unavailable option";
 
-    let newvalue = prompt(`Please enter a new value for ${newKey}`);
-    photo[newKey] = newvalue;
+    if (newKey === "userId") {
 
-    let config = {
-
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(photo)
+        let userId = prompt(`Please enter the new value for ${newKey}`);
+        let user = await getUser(userId);
+        
+        if (user === 404) {
+            alert(`Error ${user.status} => User doesn't exist`)
+            return;
+        }
+        else {
+            user[newKey] = userId
+        }
 
     }
+    else {
+        let newvalue = prompt(`Please enter the new value for ${newKey}`);
+        photo[newKey] = newvalue
+    }
 
-    let res = await fetch(`http://172.16.101.146:5803/photos/${id}`, config);
+    let config = {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(photo)
+    }
+
+    let res = await fetch(`${enlace.photo}/${id}`, config);
     let data = await res.json();
     alert("Photo value updated succesfully");
     return data;
 }
+
