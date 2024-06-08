8069cf51d1345c7c79bd9d34eeb70e854f1bc8b2
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-07 20:31:41 -0500

    feat: :sparkles: PATCH Posts

--- Js/module/posts.js
+++ Js/module/posts.js
@@ -1,9 +1,12 @@
 import { getUser } from "./user.js" 
+import { remoto, local } from "./endopoints.js"
+
+const enlace = local();
 
 // GET
 export const getPost = async(postId) => {
 
-    let res = await fetch(`http://172.16.101.146:5800/posts/${postId}`)
+    let res = await fetch(`${enlace.post}/${postId}`);
 
     if (res.status === 404) {
         return { status: 404, message: "Post doesn't exist" };
@@ -20,7 +23,7 @@ const validatePost = async({userId, title, body}) => {
     if (typeof userId !== "number" || userId === undefined) {
         return { status: 406, message: "Invalid data format: userId must be a number." };
     }   
-    let user = await getUser({ userId });
+    let user = await getUser( userId );
     if (user.status === 404) {  
         return { status: 200, message: "The User doesn't exist" };
     }
@@ -47,8 +50,9 @@ export const addPost = async(arg) => {
         headers: {"content-type": "application/json"},
         body: JSON.stringify(arg)
     }
-    let res = await fetch ("http://172.16.101.146:5800/posts", config);
+    let res = await fetch(`${enlace.post}`, config);
     let data = await res.json();
+    alert("Post added succesfully");
     return data;
 
 }
@@ -74,56 +78,67 @@ export const deletePost = async (arg) => {
 
     }
 
-    let res = await fetch(`http://172.16.101.146:5800/posts/${arg.id}`, config);
+    let res = await fetch(`${enlace.post}/${arg.id}`, config);
     if (res.status === 404) {
         return { status: 404, message: "The Post you want to delete isn't registered in the data" };
     }
 
     let data = await res.json();
-    data.status = 202;
-    data.message = `The Post ${arg.id} was deleted successfully from the database`;
+    alert("Post deleted succesfully");
     return data;
 
 }
 
-// PUT 
+// PATCH 
 export const updatePost = async(id) => {
 
     let post = await getPost(id);
-    if (post.status == 204) return "Post not found";
 
-    const key = Object.keys(post).filter(key => key !== "id");
-    let opciones = key.map((key, value) => `${value + 1}. ${key}`).join("\n");
-
-    let opc = prompt(`Available options: \n${opciones}\n Give me the option:`);
+    if (post.status == 404) {
+        alert(`Error ${post.status} => Post not found`)
+        return;
+    }
+    else {
+        
+        const key = Object.keys(post).filter(key => key !== "id");
+        let opciones = key.map((key, value) => `${value + 1}. ${key}`).join("\n");
     
-    let newKey = key[opc - 1];
-    if (!newKey) return "Unavailable option";
-
-    if (newKey === "userId") {
-
-        let newUserId = prompt(`Please enter a new value for ${newKey}`);
-        let user = await getUser(newUserId);
-
-        if (user.status === 404) {
-            alert("The User doesn't exist")
+        let opc = prompt(`Available options: \n\n${opciones}\n\n Give me the option:`);
+        
+        let newKey = key[opc - 1];
+        if (!newKey) return "Unavailable option";
+    
+        if (newKey === "userId") {
+    
+            let newUserId = prompt(`Please enter a new value for ${newKey}`);
+            let user = await getUser(newUserId);
+    
+            if (user.status === 404) {
+                alert(`Error ${user.status} => The User doesn´t exist`)
+                return;
+            }
+            else {
+                post[newKey] = newUserId;
+            }
+    
         }
-
+        else {
+            let newvalue = prompt(`Please enter a new value for ${newKey}`);
+            post[newKey] = newvalue;
+        }
+        
+        let config = {
+    
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(post)
+    
+        }
+    
+        let res = await fetch(`${enlace.post}/${id}`, config);
+        let data = await res.json();
+        alert("Post value updated succesfully");
+        return data;
+    }
     }
 
-    let newvalue = prompt(`Please enter a new value for ${newKey}`);
-    post[newKey] = newvalue;
-
-    let config = {
-
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(post)
-
-    }
-
-    let res = await fetch(`http://172.16.101.146:5800/posts/${id}`, config);
-    let data = await res.json();
-    alert("Post value updated succesfully");
-    return data;
-}
