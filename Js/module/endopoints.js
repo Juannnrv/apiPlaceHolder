535f6d2bfc1cf49abd43233421d19734ade9e028
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-07 20:27:16 -0500

    feat: :sparkles: Script para cambiar los endopoints de manera eficiente

---
+++ Js/module/endopoints.js
@@ -0,0 +1,23 @@
+
+
+
+export const local = () => ({
+
+    post: "http://172.16.101.146:5800/posts",
+    comment: "http://172.16.101.146:5801/comments",
+    album: "http://172.16.101.146:5802/albums",
+    photo: "http://172.16.101.146:5803/photos",
+    user: "http://172.16.101.146:5804/users",
+
+})
+
+
+export const remoto = () => ({
+
+    post: "https://ea9cb12e1fd707dd59cabd0bcfaca5bf.serveo.net/posts",
+    comment: "https://08c66d6fa43b0d8452d54ee3978c023b.serveo.net/comments",
+    album: "https://53977d67df4867e88ad92ace77f41d81.serveo.net/albums",
+    photo: "https://eeb0d5d633a3ac44a1c6ad64f07d43a4.serveo.net/photos",
+    user: "https://b426bc73b31fd3e606372972aae17324.serveo.net/users",
+
+})
